23b04b2d1c7b4a1489c9cc5863d017fb0aef66e7
Aditya Patwardhan <adityap.thecool@gmail.com>
2025-09-05 12:33:11 -0500

    Removed unused import in App.tsx

--- frontend/libreflash-frontend/src/App.tsx
+++ frontend/libreflash-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DeckPage from "./pages/DeckPage";
 import DeckIdPage from "./pages/DeckIdPage";
