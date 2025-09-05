3bd8c98fe36fd15c72260494f331e24460932b30
Aditya Patwardhan <adityap.thecool@gmail.com>
2025-09-05 13:04:20 -0500

    Updated import in App.tsx

--- frontend/libreflash-frontend/src/App.tsx
+++ frontend/libreflash-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DeckPage from "./pages/DeckPage";
 import DeckIdPage from "./pages/DeckIdPage";
